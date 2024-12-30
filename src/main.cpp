f0de24dcaf497b48382f5b911e9f96093968ba16
Katelyn Jensen <kj28@pop-os.home>
2024-12-29 21:25:13 -0700

    pause by clicking on screen

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,13 @@ void processInput(GLFWwindow* window) {
     glfwSetWindowShouldClose(window, true);
 }
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		videoState.paused = !videoState.paused;
+		videoState.justChanged = true;
+	}
+}
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 	if (action == GLFW_PRESS) {
 		if (key == GLFW_KEY_SPACE) {
@@ -30,7 +37,6 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 			videoState.justChanged = true;
 			adjust_hold = false;
 			adjust_skip = false;
-			std::cout << (videoState.paused ? "paused" : "playing") << std::endl;
 		}
 		if (videoState.paused) {
 			switch (key) {
@@ -95,6 +101,7 @@ int main() {
   glfwSetWindowPos(window, 100, 2000);
   glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
 
   // init GLAD
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -116,11 +123,10 @@ int main() {
 	std::cout << "I: Enter frame skip adjustment mode. Up and Down arrows will increment by 1. Max skip is 30 frames." << std::endl;
 	std::cout << "H: Enter frame hold adjustment mode. Up and Down arrows will increment by 5." << std::endl;
 
-	// timer for frame hold
 	std::chrono::steady_clock::time_point start_time;
 	std::chrono::steady_clock::time_point start_pause_time;
-	std::chrono::duration<double> duration_time = std::chrono::duration<double>(frame_hold + 1.0);
 	std::chrono::duration<double> pause_duration; 
+	std::chrono::duration<double> duration_time = std::chrono::duration<double>(frame_hold + 1.0);
 
  //-------------------------------------------------------------------------------------------------------------------------------
   while (!glfwWindowShouldClose(window)) {
@@ -136,22 +142,22 @@ int main() {
 		}
 
 		if (videoState.paused) {
-			if (videoState.justChanged) {														// just pressed pause
+			if (videoState.justChanged) {														
+				std::cout << "paused" << std::endl;
 				videoState.justChanged = false;
 				start_pause_time = std::chrono::steady_clock::now();
 			}
-
 		} else {
-			if (videoState.justChanged) {														// just pressed play
-				std::cout << "just changed to play" << std::endl;
-				auto end_pause_time = std::chrono::steady_clock::now();
+			if (videoState.justChanged) {														
+				std::cout << "playing" << std::endl;
 				videoState.justChanged = false;
-				pause_duration += end_pause_time - start_pause_time;
+				pause_duration += std::chrono::steady_clock::now() - start_pause_time;
 			}
 			
 			duration_time = std::chrono::steady_clock::now() - start_time - pause_duration;
 
-			if (!eoframes && duration_time >= std::chrono::seconds(frame_hold)) { // if it is time to change the frame (x seconds have passed)
+			// if it is time to change the frame (x seconds have passed)
+			if (!eoframes && duration_time >= std::chrono::seconds(frame_hold)) { 
 				start_time = std::chrono::steady_clock::now();
 				pause_duration = std::chrono::duration<double>::zero();
 
@@ -162,10 +168,8 @@ int main() {
 				FrameShader::Texture texture = FrameShader::imgToTexture(frame);
 				FrameShader::updateTexture(frameProgram, texture);
 				eoframes = frame.eof;
-			}																																									// all other stuff that happens when playing but not changing frame 
-
+			}																																									 
 		}
-		// TODO: fix pause duration with a justPaused variable (or do it in the key callback or separate function it can be called in mult places)
 
 		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
